4feea3951b6124b51c5c51820df4da9674227352
kurapikats <kurapikats@yahoo.com>
2017-08-30 15:37:47 +0800

    add webpack, nodemon and typescript for monitoring and auto transpile on save

---
+++ README.md
@@ -0,0 +1,5 @@
+This a very basic boilerplate for nodejs projects.
+It transpiles typescript features down to es2015.
+Current featues are:
+  * Monitors source codes then transpiles on save via webpack
+  * Monitors dist folder and reloads the new module via nodemon
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+  devtool: 'inline-source-map',
+  entry: './src/app.ts',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    // Add `.ts` and `.tsx` as a resolvable extension.
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  module: {
+    loaders: [ // loaders will work with webpack 1 or 2; but will be renamed "rules" in future
+      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+      { test: /\.tsx?$/, loader: 'ts-loader' }
+    ]
+  }
+}
